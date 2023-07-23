e7e394ca3d22edbd9eb76c49ce2885bdc08e83bb
kan kawabata <kkawabat@asu.edu>
2023-07-23 13:25:54 -0700

    minor optimization and refactoring

--- src/magic_eye_util/magic_eye_decoder.py
+++ src/magic_eye_util/magic_eye_decoder.py
@@ -1,54 +1,71 @@
 import numpy as np
 from PIL import Image, ImageOps
 from scipy.ndimage import convolve
-from scipy.signal import find_peaks, peak_widths
+from scipy.signal import find_peaks
 
 
-def decode_magic_eye(magic_image):
+def decode_magic_eye(magic_image, threshold=5, sensitivity=.8):
     bw_magic_image = ImageOps.grayscale(magic_image)
     img_arr = np.asarray(bw_magic_image, dtype='int16')
 
-    candidate_indices = get_candidate_indices(img_arr)
+    candidate_indices, candidate_weights = get_candidate_indices(img_arr)
 
     result_depth_map_arr = np.zeros(img_arr.shape)
-    depth_range = np.linspace(128, 256, len(candidate_indices))
-
-    weights = np.array([[1, 1, 1],
-                        [1, 2, 1],
-                        [1, 1, 1]], dtype=float)
-    weights = weights / np.sum(weights[:])
+    depth_range = np.linspace(64, 192, len(candidate_indices))
 
     for i, (shift_idx, shift_diff) in enumerate(get_shift_diff(candidate_indices, img_arr)):
+        print(shift_idx)
         shift_diff = np.roll(shift_diff, shift_idx, axis=1)
-        diff_zeros_smoothed = convolve(abs(shift_diff) < 5, weights, mode='constant')
-        result_depth_map_arr[diff_zeros_smoothed] = depth_range[i]
+        shift_diff_smoothed = smooth_image(abs(shift_diff) < threshold)
+        activated_depth_pixels = shift_diff_smoothed >= sensitivity
+        result_depth_map_arr[activated_depth_pixels] = depth_range[i]
     img = Image.fromarray(result_depth_map_arr)
+    img.show()
     return img
 
 
 def get_candidate_indices(img_arr):
+    # this function returns a list of indices that highly correlates alone the x-axis used to determine the tiling intervals of the magic eye image
     shift_zero_count = []
-    for shift_idx, shift_diff in get_shift_diff(range(img_arr.shape[1]//2), img_arr):
+    for shift_idx, shift_diff in get_shift_diff(range(img_arr.shape[1] // 2), img_arr):
         shift_zero_count.append(abs(shift_diff).sum())
-    indicies = get_high_correlation_indicies(np.array(shift_zero_count))
+    indices, weights = get_high_correlation_indices(np.array(shift_zero_count))
+    return indices, weights
 
-    return indicies
 
-
-def get_high_correlation_indicies(x):
+def get_high_correlation_indices(x):
     n = x.size
     norm = (x - np.mean(x))
     result = np.correlate(norm, norm, mode='same')
-    acorr = result[n//2 + 1:] / (x.var() * np.arange(n-1, n//2, -1))
+    acorr = result[n // 2 + 1:] / (x.var() * np.arange(n - 1, n // 2, -1))
     peaks, _ = find_peaks(acorr, height=.1)
     if len(peaks) == 0:
         raise Exception("could not decode magic eye image")
     best_peak = sorted(peaks, key=lambda x: acorr[x], reverse=True)[0]
-    return range(max(int(best_peak-(best_peak/2)), 0), int(best_peak+(best_peak/2)))
+    indices = range(max(int(best_peak - (best_peak / 2)), 0), int(best_peak + (best_peak / 2)))
+    indices_weights = acorr[indices]
+    return indices, indices_weights
 
 
 def get_shift_diff(shift_range, img_arr):
     for shift_idx in shift_range:
-        print(shift_idx)
         shift_diff = img_arr - np.roll(img_arr, -shift_idx, axis=1)
         yield shift_idx, shift_diff
+
+
+SMOOTHING_WEIGHTS = np.array([[.1, .1, .1],
+                              [.1, .2, .1],
+                              [.1, .1, .1]], dtype=float)
+
+
+def smooth_image(img_arr):
+    smooth_img_arr = convolve(img_arr.astype(float), SMOOTHING_WEIGHTS, mode='constant')
+    return smooth_img_arr
+
+
+def running_avg(cur_avg, new_frame, i):
+    if cur_avg is None:
+        return new_frame
+
+    new_avg = cur_avg * (i / (i + 1)) + (new_frame / (i + 1))
+    return new_avg
--- src/magic_eye_util/magic_eye_generator.py
+++ src/magic_eye_util/magic_eye_generator.py
@@ -3,53 +3,47 @@ from PIL import Image
 
 
 def generate_magic_eye(depth_map_image, texture_map_image, depth_factor=.1, num_strips=10, strip_width=100):
-    texture_map_arr = resize_texture_img(texture_map_image, strip_width)
-    depth_map_arr = resize_depth_map(depth_map_image, num_strips, strip_width)
-    result_map = gen_depth_offset_map(texture_map_arr, depth_map_arr, num_strips, depth_factor)
-    return Image.fromarray(result_map, mode="RGB")
+    texture_arr = resize_texture_img(texture_map_image, strip_width)
+    depth_arr = resize_depth_map(depth_map_image, num_strips, strip_width)
+    magic_eye_arr = gen_depth_offset_map(texture_arr, depth_arr, num_strips, depth_factor)
+    return Image.fromarray(magic_eye_arr, mode="RGB")
 
 
-def resize_texture_img(texture_map_im, strip_width):
-    w, h = texture_map_im.size
-    r = h / w
-    texture_map_resized = texture_map_im.resize((strip_width, int(strip_width * r)))
-    texture_map_data = np.array(texture_map_resized)
-    return texture_map_data
+def resize_texture_img(img, strip_width):
+    w, h = img.size
+    img_resized = img.resize((strip_width, int(strip_width * (h / w))))
+    arr_resized = np.array(img_resized)
+    if len(arr_resized.shape) == 2:
+        arr_resized = arr_resized[:, :, None]
+    return arr_resized
 
 
-def resize_depth_map(depth_map_im, num_strips, strip_width):
-    bw_depth_map_image = depth_map_im.convert('L')
-    w, h = bw_depth_map_image.size
-    r = h / w
-    depth_map_resized = bw_depth_map_image.resize((strip_width * num_strips, int(strip_width * num_strips * r)), resample=Image.BOX)
-    depth_map_data = np.array(depth_map_resized) / 255
-    return depth_map_data
+def resize_depth_map(img, num_strips, strip_width):
+    bw_img = img.convert('L')
+    w, h = bw_img.size
+    bw_img_resized = bw_img.resize((strip_width * num_strips, int(strip_width * num_strips * (h / w))), resample=Image.BOX)
+    arr_resized = np.array(bw_img_resized) / 255
+    return arr_resized
 
 
-def gen_depth_offset_map(texture_map_data, depth_map_data, num_strips, depth_factor):
-    if len(texture_map_data.shape) == 2:
-        num_texture_channels = 1
-    elif len(texture_map_data.shape) == 3:
-        num_texture_channels = texture_map_data.shape[2]
-    else:
-        raise ValueError("expected at most 3 dimension for texture_map (width, height, #channel)")
-
-    th, tw = texture_map_data.shape[0:2]
-    dh, dw = depth_map_data.shape[0:2]
-    depth_normed = (depth_map_data * depth_factor * tw).astype(int)
+def gen_depth_offset_map(texture_arr, depth_arr, num_strips, depth_factor):
+    th, tw, t_channels = texture_arr.shape[0:3]
+    dh, dw = depth_arr.shape[0:2]
+    depth_normed = (depth_arr * depth_factor * tw).astype(int)
 
     tile_range_x = np.tile(range(tw), (dh, 1))
     tile_range_y = np.tile(range(th), (tw, int(dh / th) + 1)).T[:dh, :]
 
-    result_map = np.empty((dh, tw * (num_strips + 1), num_texture_channels))
+    result_map = np.empty((dh, tw * (num_strips + 1), t_channels))
+    result_map[:, :tw, :] = texture_arr[tile_range_y, tile_range_x, :]
+
     tile_range_x_tmp = np.tile(range(result_map.shape[1]), (dh, 1))
     tmp = np.empty((dh, tw * (num_strips + 1)))
-    result_map[:, :tw, :] = texture_map_data[tile_range_y, tile_range_x, :]
 
     row_idc = np.arange(result_map.shape[0])
 
     for i in range(dw):
-        result_map[row_idc, i + tw, :] = result_map[row_idc, i + depth_normed[:, i], :].reshape(-1, num_texture_channels)
+        result_map[row_idc, i + tw, :] = result_map[row_idc, i + depth_normed[:, i], :].reshape(-1, t_channels)
         tmp[row_idc, i + tw] = tile_range_x_tmp[row_idc, i + depth_normed[:, i]]
 
     result_map = result_map[:, :, :3].astype(np.uint8)
