3ed13c434fc3637d9f78f6e3d39a128604579de9
kan kawabata <kkawabat@asu.edu>
2023-07-23 00:25:18 -0700

    minor optimization of decoding algorithm

--- src/magic_eye_util/magic_eye_decoder.py
+++ src/magic_eye_util/magic_eye_decoder.py
@@ -1,53 +1,66 @@
 import numpy as np
 from PIL import Image, ImageOps
 from scipy.ndimage import convolve
+from scipy.signal import find_peaks, peak_widths
 
 
 def decode_magic_eye(magic_image):
     bw_magic_image = ImageOps.grayscale(magic_image)
-    img_arr = np.asarray(bw_magic_image)
+    img_arr = np.asarray(bw_magic_image, dtype='int16')
 
-    depth_width = estimate_depth_width(img_arr)
+    candidate_indices = get_candidate_indices(img_arr)
 
     result_depth_map_arr = np.zeros(img_arr.shape)
-    depth_range = np.linspace(0, 256, depth_width-1)
-    for shift_idx, diff_zeros_smoothed in get_depth_zeros(range(depth_width-1), img_arr):
-        result_depth_map_arr[diff_zeros_smoothed] = depth_range[shift_idx]
+    depth_range = np.linspace(128, 256, len(candidate_indices))
 
-    return Image.fromarray(result_depth_map_arr.astype(np.uint8))
-
-
-def estimate_depth_width(img_arr):
-    shift_zero_count = []
-
-    for shift_idx, diff_zeros_smoothed in get_depth_zeros(range(img_arr.shape[1]), img_arr):
-        shift_zero_count.append(sum(sum(diff_zeros_smoothed)))
-    return autocorr(np.array(shift_zero_count))
-
-
-def autocorr(x):
-    n = x.size
-    norm = (x - np.mean(x))
-    result = np.correlate(norm, norm, mode='same')
-    acorr = result[n//2 + 1:] / (x.var() * np.arange(n-1, n//2, -1))
-    lag = np.abs(acorr).argmax() + 1
-    r = acorr[lag-1]
-    if np.abs(r) > 0.5:
-        return lag
-    raise Exception('Could not decode image')
-
-
-def get_depth_zeros(depth_range, img_arr):
     weights = np.array([[1, 1, 1],
                         [1, 2, 1],
                         [1, 1, 1]], dtype=float)
     weights = weights / np.sum(weights[:])
 
-    for shift_idx, depth in enumerate(depth_range):
+    for i, (shift_idx, shift_diff) in enumerate(get_shift_diff(candidate_indices, img_arr)):
+        diff_zeros_smoothed = convolve(shift_diff == 0, weights, mode='constant')
+        result_depth_map_arr[diff_zeros_smoothed] = depth_range[i]
+        Image.fromarray(shift_diff).show()
+        # Image.fromarray(diff_zeros_smoothed).show()
+    img = Image.fromarray(result_depth_map_arr.astype(np.uint8))
+    return img
+
+
+def get_candidate_indices(img_arr):
+    shift_zero_count = []
+    for shift_idx, shift_diff in get_shift_diff(range(img_arr.shape[1]//2), img_arr):
+        shift_zero_count.append(abs(shift_diff).sum())
+    indicies = get_high_correlation_indicies(np.array(shift_zero_count))
+
+    return indicies
+
+
+def get_high_correlation_indicies(x):
+    n = x.size
+    norm = (x - np.mean(x))
+    result = np.correlate(norm, norm, mode='same')
+    acorr = result[n//2 + 1:] / (x.var() * np.arange(n-1, n//2, -1))
+    peaks, _ = find_peaks(acorr, height=.1)
+    if len(peaks) == 0:
+        raise Exception("could not decode magic eye image")
+    best_peak = sorted(peaks, key=lambda x: acorr[x], reverse=True)[0]
+    return range(max(int(best_peak-(best_peak/2)), 0), int(best_peak+(best_peak/2)))
+    # _, _, l, r = peak_widths(acorr, [best_peak])
+    # return range(max(int(l[0]-(best_peak/2)), 0), int(r[0]+(best_peak/2)))
+    # lag = np.abs(acorr).argmax() + 1
+    # r = acorr[lag-1]
+    # if np.abs(r) > 0.5:
+    #     return range(1, lag)
+    # raise Exception('Could not decode image')
+
+
+def get_shift_diff(shift_range, img_arr):
+
+    for shift_idx in shift_range:
         print(shift_idx)
-        diff_zeros = (img_arr - np.roll(img_arr, -shift_idx, axis=1)) == 0
-        diff_zeros_smoothed = convolve(diff_zeros, weights, mode='constant')
-        yield shift_idx, diff_zeros_smoothed
+        shift_diff = img_arr - np.roll(img_arr, -shift_idx, axis=1)
+        yield shift_idx, shift_diff
 
 
 
